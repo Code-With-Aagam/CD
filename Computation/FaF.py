c6ece83b130f603c9e6b006eaf4824a03cb79c35
Aagam Shah <91781985+Code-With-Aagam@users.noreply.github.com>
2022-04-04 19:30:12 +0530

    Add files via upload

---
+++ Computation/FaF.py
@@ -0,0 +1,24 @@
+import sys
+sys.setrecursionlimit(60)
+
+def first(string):
+    #print("first({})".format(string))
+    first_ = set()
+    if string in non_terminals:
+        alternatives = productions_dict[string]
+
+        for alternative in alternatives:
+            first_2 = first(alternative)
+            first_ = first_ |first_2
+
+    elif string in terminals:
+        first_ = {string}
+
+    elif string=='' or string=='@':
+        first_ = {'@'}
+
+    else:
+        first_2 = first(string[0])
+        if '@' in first_2:
+            i = 1
+            while '@' in first_2:
